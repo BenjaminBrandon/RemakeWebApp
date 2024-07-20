8031a54f4a50373efbc984c8e3a9d81aeaed9b1d
Benjamin Brandon <benb89.bb@gmail.com>
2024-07-20 17:11:59 -0400

    base structure for all components

--- src/components/collections.jsx
+++ src/components/collections.jsx
@@ -2,15 +2,15 @@ import { React } from 'react';
 
 const Collections = () => {
   return (
-    <div className='collections'>
+    <div className="collections">
       <h2>Collections</h2>
       <h3>
         Explore a wide
-        <br>
+        <br></br>
         variety of collections.
       </h3>
     </div>
-  )
-}
+  );
+};
 
 export default Collections;
---
+++ src/components/courses.jsx
@@ -0,0 +1,16 @@
+import { React } from 'react';
+
+const Courses = () => {
+  return (
+    <div className="courses">
+      <h2>Academy</h2>
+      <h3>
+        Learn from the
+        <br></br>
+        best instructors.
+      </h3>
+    </div>
+  );
+};
+
+export default Courses;
