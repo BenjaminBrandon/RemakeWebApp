be05b4137b1eb90ba0e3d8d58e1da24c87d8e1d3
Benjamin Brandon <benb89.bb@gmail.com>
2024-07-21 16:38:43 -0400

    adding main home page basics

---
+++ src/components/sotd.jsx
@@ -0,0 +1,13 @@
+import { React } from 'react';
+
+const Sotd = () => {
+  return (
+    <div className="Sotd">
+      <h3>Site of the Day - insert todays date</h3>
+      <h1>NAUGHTYDUCK</h1>
+      <h2>insert link to users profile page</h2>
+    </div>
+  );
+};
+
+export default Sotd;
