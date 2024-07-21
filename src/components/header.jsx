bbda5de260182ca17f2312028d374f25887c7065
Benjamin Brandon <benb89.bb@gmail.com>
2024-07-21 16:43:06 -0400

    adding header basics

---
+++ src/components/header.jsx
@@ -0,0 +1,18 @@
+import { React } from 'react';
+
+const Header = () => {
+  return (
+    <div className="Header">
+      <div>
+        <h3> drop down menu</h3>
+        <h3>search bar</h3>
+        <button>Login</button>
+        <button>Sign Up</button>
+        <button>Be Pro</button>
+        <button>Submit Website</button>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
