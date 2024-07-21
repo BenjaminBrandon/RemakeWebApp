df1dc4c5e2912d160a80080f013ab3209db057a8
Benjamin Brandon <benb89.bb@gmail.com>
2024-07-21 16:47:55 -0400

    adding marquee basics

---
+++ src/components/marquee.jsx
@@ -0,0 +1,14 @@
+import { React } from 'react';
+
+//repeating left directional scroll anchor
+const Marquee = () => {
+  return (
+    <div className="Marquee">
+      <h3>
+        Sun,Fun, and Learning //insert smile emote// all courses for Only $9.99
+      </h3>
+    </div>
+  );
+};
+
+export default Marquee;
